4f9d3edcf56ee2e6537c9b549d7730264bf2f28e
vimal-11 <vimalaadith.bs@gmail.com>
2023-08-05 02:04:19 +0530

    mysql connection

--- MoneyTreeWeb/settings.py
+++ MoneyTreeWeb/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'MoneyTreeWeb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', 'localhost'),
+        'PORT': config('DB_PORT', ''),
     }
 }
 
